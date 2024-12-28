0078f21c5bfaf7cd0940a658560146da2fa90168
@LAKA <lakalakazy99@gmail.com>
2024-12-28 21:45:02 +0530

    ddos_simulation.py

--- ddos_simulation.py
+++ ddos_simulation.py
@@ -16,8 +16,9 @@ def main():
     print("NO SYSTEM IS SAFE")
     print("Clay was here")
 
-    ip = input("Enter the target IP address: ")
-    port = input("Enter the target port: ")
+    ip_port = input("Enter the target IP address and port (e.g., 51.79.249.206:22003): ")
+    ip, port = ip_port.split(':')
+
     packets = input("Enter the number of packets to send: ")
 
     try:
